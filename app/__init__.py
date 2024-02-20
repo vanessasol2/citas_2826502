14b0701ecc96fa7f32c8b55fe6efcf97cb34a9a9
unknown <Administrador@BOGDFPCGMF4136.FormacionSena.RED>
2024-02-20 17:25:34 -0500

    migraciones

---
+++ app/__init__.py
@@ -0,0 +1,24 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+##importr config 
+from .config import Config
+
+##crear objeto de app
+app = Flask(__name__)
+
+##configura el objeto flask
+app.config.from_object(Config)
+
+##objeto SQLAlchemy
+db = SQLAlchemy(app)
+
+#objeto para las migraciones
+migrate = Migrate(app, db)
+
+#importar los modelos
+from .models import Medico
+
+##ejecutar el objeto
+if __name__== "__main__":
+    app.run()
---
+++ app/models.py
@@ -0,0 +1,16 @@
+#necesitamos a sqlAlchemy
+#Definir los atributos de objeto 
+#pero con tipos traducibles a SQL y mysql
+
+from app import db
+
+class Medico(db.Model):
+    
+    id = db.Column(db.Integer, primary_key = True)
+    nombre = db.Column(db.String(120), nullable = True)
+    apellido = db.Column(db.String(120), nullable = True)
+    tipo_identificacion = db.Column(db.String(4), nullable = True)
+    numero_identificacion = db.Column(db.Integer)
+    registro_medico = db.Column(db.Integer)
+    especialidad = db.Column(db.String(50))
+    
