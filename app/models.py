841b4ccd3e1d612d5ffc13f618fb345a558e16f4
David <davidm435465123@gmail.com>
2024-03-06 14:34:11 -0500

    evidencia_3

--- app/models.py
+++ app/models.py
@@ -46,6 +46,7 @@ class Cita(db.Model):
     paciente_id = db.Column (db.Integer,db.ForeignKey("pacientes.id"))
     medico_id = db.Column (db.Integer,db.ForeignKey("medicos.id"))
     consultorio_id = db.Column (db.Integer,db.ForeignKey("consultorios.id"))
+    valor = db.Column(db.Integer)
     
      
                             
