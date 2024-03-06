5711ced28313f3779753f6dd06179dffe2314a47
Mauricio Villanueva <freddymauriciovillanuevasilva@gmail.com>
2024-03-05 19:59:33 -0500

    comentarios

--- app/models.py
+++ app/models.py
@@ -39,4 +39,18 @@ class Cita(db.Model):
     consultorio_id = db.Column(db.Integer, db.ForeignKey('consultorios.id'))
     
     
-    
+'''    
+med1 = Medico(nombres = 'gonzalo', apellidos = 'Fernandez', tipo_identificacion = 'CC', numero_identificacion = 123455, registro_medico = 8171635, especialidad ='cardiologo')
+med2 = Medico(nombres = 'Sandra', apellidos = 'Vargas', tipo_identificacion = 'CC', numero_identificacion = 4163839, registro_medico = 765456, especialidad ='Urologa')
+pac1 = Paciente(nombres = 'Maria', apellidos = 'Martinez', tipo_identificacion = 'CC', numero_identificacion = 234577,altura = 159, tipo_sangre = 'O+' )
+pac2 = Paciente(nombres = 'Juan', apellidos = 'Romero', tipo_identificacion = 'CC', numero_identificacion = 876568,altura = 180, tipo_sangre = 'A-' )
+pac3 = Paciente(nombres = 'Augusto', apellidos = 'villalba', tipo_identificacion = 'CC', numero_identificacion = 896383,altura = 173, tipo_sangre = 'O+' )
+con1 = Consultorio(numero = 102)
+con2 = Consultorio(numero = 203)
+con3 = Consultorio(numero = 206)
+mi_cita = Cita(fecha = '2024-03-05', paciente_id = 1, medico_id = 1, consultorio_id = 1 )
+fecha1 = datetime(2024, 03, 05)
+
+for ci in Cita.query.all():        
+    print('fecha: '+str(ci.fecha)+' Paciente identificación: '+ str(ci.paciente.numero_identificacion)+ ',paciente nombre: ' + ci.paciente.nombres+' '+ ci.paciente.apellidos)
+'''
