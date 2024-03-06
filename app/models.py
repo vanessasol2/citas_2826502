84a7479248b4165491c56250e97a1c0f65fd7e50
Mauricio Villanueva <freddymauriciovillanuevasilva@gmail.com>
2024-03-05 20:06:56 -0500

    atributo valor

--- app/models.py
+++ app/models.py
@@ -37,6 +37,7 @@ class Cita(db.Model):
     paciente_id = db.Column(db.Integer, db.ForeignKey('pacientes.id'))
     medico_id = db.Column(db.Integer, db.ForeignKey('medicos.id'))
     consultorio_id = db.Column(db.Integer, db.ForeignKey('consultorios.id'))
+    valor = db.Column(db.Integer)
     
     
 '''    
