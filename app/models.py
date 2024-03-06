07bc6b39157c22b8cd3f1c3e38cf2f3d5b32a5b4
MillerMora <millermora2022@gmail.com>
2024-03-06 14:28:57 -0500

    3 casteo

--- app/models.py
+++ app/models.py
@@ -14,6 +14,8 @@ class Medico(db.Model):
     numero_identificacion = db.Column(db.Integer)
     registro_medico = db.Column(db.Integer)
     especialidad = db.Column(db.String(50))
+
+    citas = db.relationship("Cita" , backref = "medico" )
     
 class Paciente(db.Model):
     __tablename__ = 'pacientes'
@@ -25,10 +27,14 @@ class Paciente(db.Model):
     altura = db.Column(db.Integer)
     tipo_sangre = db.Column(db.String(2))
 
+    citas = db.relationship("Cita" , backref = "paciente" )
+
 class Consultorio(db.Model):
     __tablename__ = 'consultorios'
     id = db.Column(db.Integer, primary_key = True)
     numero = db.Column(db.Integer)
+
+    citas = db.relationship("Cita" , backref = "consultorio" )
     
 class Cita(db.Model):
     __tablename__ = 'citas'
@@ -42,8 +48,8 @@ class Cita(db.Model):
     
 '''    
 med1 = Medico(nombres = 'gonzalo', apellidos = 'Fernandez', tipo_identificacion = 'CC', numero_identificacion = 123455, registro_medico = 8171635, especialidad ='cardiologo')
-med2 = Medico(nombres = 'Sandra', apellidos = 'Vargas', tipo_identificacion = 'CC', numero_identificacion = 4163839, registro_medico = 765456, especialidad ='Urologa')
-pac1 = Paciente(nombres = 'Maria', apellidos = 'Martinez', tipo_identificacion = 'CC', numero_identificacion = 234577,altura = 159, tipo_sangre = 'O+' )
+med2 = Medico(nombre = 'Sandra', apellido = 'Vargas', tipo_identificacion = 'CC', numero_identificacion = 4163839, registro_medico = 765456, especialidad ='Urologa') 
+pac1 = Paciente(nombre = 'Maria', apellido = 'Martinez', tipo_identificacion = 'CC', numero_identificacion = 234577, altura = 159, tipo_sangre = 'O+' ) 
 pac2 = Paciente(nombres = 'Juan', apellidos = 'Romero', tipo_identificacion = 'CC', numero_identificacion = 876568,altura = 180, tipo_sangre = 'A-' )
 pac3 = Paciente(nombres = 'Augusto', apellidos = 'villalba', tipo_identificacion = 'CC', numero_identificacion = 896383,altura = 173, tipo_sangre = 'O+' )
 con1 = Consultorio(numero = 102)
@@ -52,6 +58,9 @@ con3 = Consultorio(numero = 206)
 mi_cita = Cita(fecha = '2024-03-05', paciente_id = 1, medico_id = 1, consultorio_id = 1 )
 fecha1 = datetime(2024, 03, 05)
 
+
+cita1 = Cita(fecha = fecha1, paciente_id = 1, medico_id = 1, consultorio_id = 1, valor = valor1)      
+
 for ci in Cita.query.all():        
-    print('fecha: '+str(ci.fecha)+' Paciente identificación: '+ str(ci.paciente.numero_identificacion)+ ',paciente nombre: ' + ci.paciente.nombres+' '+ ci.paciente.apellidos)
+    print('fecha: '+str(ci.fecha)+' Paciente identificación: '+ str(ci.paciente.numero_identificacion)+ ',paciente nombre: ' + ci.paciente.nombre+' '+ ci.paciente.apellido+ ' valor cita: '+ str(ci.valor))
 '''
