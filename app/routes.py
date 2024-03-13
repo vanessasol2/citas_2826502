2acf04b0d101b812fed11184201b0e2892921f11
Daniel Solano <wdsp9898@gmail.com>
2024-03-13 09:41:57 -0500

    formulario

--- app/routes.py
+++ app/routes.py
@@ -30,12 +30,6 @@ def get_medico_by_id(id):
     return render_template("medico.html",
                             med = medico )
 
-@app.route("/pacientes/<int:id>")
-def get_paciente_by_id(id):
-    paciente = Paciente.query.get(id)
-    return render_template("paciente.html",
-                           pac = paciente )
-
 #crear ruta para crear nuevo medico
 @app.route("/medicos/create" , methods = [ "GET" , "POST"] )
 def create_medico():
@@ -67,3 +61,44 @@ def create_medico():
         db.session.add(new_medico)
         db.session.commit()
         return "medico registrado"
+
+@app.route("/pacientes/<int:id>")
+def get_paciente_by_id(id):
+    paciente = Paciente.query.get(id)
+    return render_template("paciente.html",
+                            pac = paciente )
+    
+@app.route("/pacientes/create" , methods = [ "GET" , "POST"] )
+def create_paciente():
+    ####### mostrar el formulario : get ###########
+    if( request.method == "GET" ):
+        #el usuario ingreso con navegador con https://localhost.....
+        tipo_sangre = [
+            "A+",
+            "O+",
+            "B+",
+            "AB+",
+            "A-",
+            "O-",
+            "B-",
+            "AB-"
+        ]
+        return render_template("paciente_form.html",
+                            tipo_sangre = tipo_sangre)
+
+#### Cuando el usuario presiona el boton de guardar#####
+#### los datos del formulario viajan al servidor
+    
+    elif(request.method == "POST"):
+        #cuando se presiona "guardar"
+        new_paciente = Paciente(nombre = request.form["nombre"],
+                            apellidos = request.form["apellidos"],
+                            tipo_identificacion = request.form["ti"],
+                            numero_identificacion = request.form["ni"],
+                            altura = request.form["al"],
+                            tipo_de_sangre = request.form["ts"]
+                            )
+        #añadirlo a la sesion sqlalchemy
+        db.session.add(new_paciente)
+        db.session.commit()
+        return "paciente registrado"
